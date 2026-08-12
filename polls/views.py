253fe2d29aaef991f9d7fea0af3a0202472c6c37
souad <souadquzmar13@gmail.com>
2026-08-12 17:27:42 +0300

    add index view

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from polls.models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    questions = Question.objects.order_by('-pub_date')[:5]
+    context = {"latest_question_list": questions}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s"%question_id)
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s"%question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
