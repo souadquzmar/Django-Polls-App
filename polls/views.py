947367be517d61dea6d87f9bfe691db9696b28c0
souad <souadquzmar13@gmail.com>
2026-08-13 10:30:00 +0300

    add voting feature

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,11 @@
+from django.db.models import Model, F
 from django.http import HttpResponse
-from django.http.response import Http404
+from django.http.response import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -16,7 +18,16 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {"question" :question})
 
 def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s"%question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,'polls/detail.html', {"question":question, "error_message":"You didn't select a choice."})
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
