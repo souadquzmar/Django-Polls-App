3636365bd0d37ed77f09d66fbc1b93b4694488c6
souad <souadquzmar13@gmail.com>
2026-08-13 10:41:04 +0300

    refactor code by using generic views

--- polls/views.py
+++ polls/views.py
@@ -4,22 +4,27 @@ from django.http.response import Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from polls.models import Question, Choice
 
 
-def index(request):
-    questions = Question.objects.order_by('-pub_date')[:5]
-    context = {"latest_question_list": questions}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {"question" :question})
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
